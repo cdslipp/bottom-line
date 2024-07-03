17a544e8c420b4d26b27a8c9b45c632f40dbc59f
Cameron Slipp <cdslipp@gmail.com>
2024-07-02 22:49:17 -0400

    Initial commmit

---
+++ src/lib/CategoryLine.svelte
@@ -0,0 +1,23 @@
+<script>
+	export let transaction;
+</script>
+
+<div class="transaction-item">
+	<span>{transaction.date}</span>
+	<span>{transaction.description}</span>
+	<span>${transaction.subtotal}</span>
+	<span>${transaction.salesTax}</span>
+	<span>${transaction.total}</span>
+</div>
+
+<style>
+	.transaction-item {
+		display: flex;
+		justify-content: space-between;
+		padding: 0.5rem 0;
+	}
+
+	.transaction-item:nth-child(odd) {
+		background-color: #f5f5f5;
+	}
+</style>
